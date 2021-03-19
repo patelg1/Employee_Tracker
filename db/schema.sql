07df4c566df82c17119a8513209f842e136c8462
Gopal Patel <patelgopalk1@gmail.com>
2021-03-19 19:13:55 -0400

    updated foreign keys

--- db/schema.sql
+++ db/schema.sql
@@ -4,24 +4,24 @@ CREATE DATABASE employee_trackerDB;
 USE employee_trackerDB;
 
 CREATE TABLE department (
-    id INT PRIMARY KEY,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(30)
 );
 
 CREATE TABLE role (
-    id INT PRIMARY KEY,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30),
-    salary DECIMAL(10,4),
+    salary DECIMAL,
     department_id INT,
-    FOREIGN KEY (department_id) REFERENCES department_id ON DELETE CASCADE
+    FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE CASCADE
 );
 
 CREATE TABLE employee (
-    id INT PRIMARY KEY,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30),
     last_name VARCHAR(30),
     role_id INT,
     manager_id INT,
-    FOREIGN KEY (role_id) REFERENCES role_id ON DELETE CASCADE,
-    FOREIGN KEY (manager_id) REFERENCES employee_id ON DELETE CASCADE
+    FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE CASCADE,
+    FOREIGN KEY (manager_id) REFERENCES employee(id)
 );
